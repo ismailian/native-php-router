ccaee806712bed4e72bc9e3c34c4b74dd2094699
Ismail Aatif <techcyber598@gmail.com>
2022-02-17 15:03:13 +0000

    Cleaned code

--- src/FlexRouter.php
+++ src/FlexRouter.php
@@ -13,13 +13,6 @@ class FlexRouter extends Router
 {
 
     /**
-     * default constructor
-     */
-    public function __construct()
-    {
-    }
-
-    /**
      * the magic __call method.
      * it helps call http request methods as router class methods dynamically.
      * 
@@ -40,16 +33,5 @@ class FlexRouter extends Router
             'method' => strtoupper($name),
             'route'  => $route,
         ], $callback);
-    }
-
-    /**
-     * default destructor.
-     * it takes care of unhandled routes.
-     */
-    public function __destruct()
-    {
-        if (!$this->handled) {
-            die("Unhandled Route");
-        }
     }
 }
